9a955181992fa6251e15f4be327e6692fb018841
Marius PAQUET <65286048+MariusPaq@users.noreply.github.com>
2020-10-08 21:52:31 +0200

    Delete main.js

--- defisJSBo/main.js
+++
@@ -1,8 +0,0 @@
-const myBtn = document.getElementById('myBtn') ;
-const cntClic = document.getElementById('cntClic');
-var nbClic = 0;
-console.log('you clicked '+nbClic+' times');
-myBtn.addEventListener("click",function(){
-  nbClic += 1;
-  console.log('you clicked '+nbClic+' times');
-})
